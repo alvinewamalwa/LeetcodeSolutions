3389b137c8d5d91f446e86b74b01f2ea50d0e120
alvinewamalwa <wamalwaalvine@gmail.com>
2025-06-03 21:52:44 +0300

    Sqrt(x)

---
+++ C++/15- Sqrt(x).cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+
+        string result;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while(i < a.length() || j < b.length()) {
+
+            int sum = carry;
+            if(i < a.length())
+                sum += a[i--] - '0';
+            if(j < b.length())
+                sum += b[j--] -'0';
+
+            carry = sum > 1 ? 1 : 0;
+            result += to_string(sum%2);
+
+        }
+        if(carry) result += to_string(carry);
+        reverse(result.begin(), result.end());
+
+        return result;
+
+        
+    }
+};
